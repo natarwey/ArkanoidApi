75970ee382248a78b011069c680a8f1fc633e6c3
user <user@DESKTOP-RA6T6M6>
2025-03-19 21:04:55 +0300

    работает

---
+++ ArkanoidApi/Migrations/20250316142845_home.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArkanoidApi.Migrations
+{
+    public partial class home : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
